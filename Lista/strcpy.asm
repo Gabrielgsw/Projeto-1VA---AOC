160dc34c9a9952ed8f52b2d2d2079f27a2964a41
Gabrielgsw <ggermanow279@gmail.com>
2025-05-31 21:07:47 -0300

    strcpy finalizado

--- Lista/strcpy.asm
+++ Lista/strcpy.asm
@@ -1,5 +1,37 @@
+.data
+	string: .asciiz "Palavra"
+	dest:  .space 7 #aloca os bytes necessários para armazenar a string 
+	
 .text
 
-.data
+  main:
 
- main:
+    la $a0, dest       # carrega o endereço de destino
+    la $a1, string     # carrega o endereço de origem
+    jal strcpy         # chama o label
+
+    li $v0, 4          # imprime string
+    la $a0, dest
+    syscall
+
+    li $v0, 10         # finaliza programa
+    syscall
+
+strcpy:
+	add $t0,$a1,$0 #aramazenando em t0 o endereço de origem
+	add $t1,$a0,$0 #armazenando em t1 o endereço de destino
+	
+	
+	for:
+	   lb $t2,0($t0) #carrega o caractere atual do loop
+	   sb $t2,0($t1) #armazena em t2 o caractere atual do loop
+	   beq $t2,$0,end #encerra o loop caso o caracete atual seja o \n
+	   addi $t0, $t0, 1 #Incrementado ponteiro origem
+           addi $t1, $t1, 1#Incrementado ponteiro destino
+           j for #Reinicia loop
+           
+        end:
+        	jr $ra 
+        	
+        	    
+        	        
