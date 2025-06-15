7d2144088eb49f9d6af6f082d211627075d53ba9
Wellington Viana <124603444+UelitoWV@users.noreply.github.com>
2025-06-15 17:43:36 -0300

    Adição de novas funções no utils

--- Projeto/utils.asm
+++ Projeto/utils.asm
@@ -1,41 +1,56 @@
 .data
+	int_to_str_buffer: .space 4
+	
+	# Nome dos comandos
+	ad_morador: .asciiz "ad_morador"
+	ad_auto: .asciiz "ad_auto"
+	rm_morador: .asciiz "rm_morador"
+	rm_auto: .asciiz "rm_auto"
+	
+	info_ap: .asciiz "info_ap"
+	limpar_ap: .asciiz "limpar_ap"
+	# info_geral: .asciiz "info_geral"
+	
+	salvar: .asciiz "salvar"
+	recarregar: .asciiz "recarregar"
+	formatar: .asciiz "formatar"
+
 
 
 # Salva os registradores
 .macro stack_reg
-    addi $sp, $sp, -48
-    sw $t0, 0($sp)
-    sw $t1, 4($sp)
-    sw $t2, 8($sp)
-    sw $t3, 12($sp)
-    sw $t4, 16($sp)
-    sw $t5, 20($sp)
-    sw $t6, 24($sp)
-    sw $t7, 28($sp)
-    sw $a0, 32($sp)
-    sw $a1, 36($sp)
-    sw $a2, 40($sp)
-    sw $a3, 44($sp)
+    	addi $sp, $sp, -48
+    	sw $t0, 0($sp)
+    	sw $t1, 4($sp)
+    	sw $t2, 8($sp)
+    	sw $t3, 12($sp)
+    	sw $t4, 16($sp)
+    	sw $t5, 20($sp)
+    	sw $t6, 24($sp)
+    	sw $t7, 28($sp)
+    	sw $a0, 32($sp)
+    	sw $a1, 36($sp)
+    	sw $a2, 40($sp)
+    	sw $a3, 44($sp)
 .end_macro
 
 # Esvazia/recupera os registradores
 .macro unstack_reg
-    lw $t0, 0($sp)
-    lw $t1, 4($sp)
-    lw $t2, 8($sp)
-    lw $t3, 12($sp)
-    lw $t4, 16($sp)
-    lw $t5, 20($sp)
-    lw $t6, 24($sp)
-    lw $t7, 28($sp)
-    lw $a0, 32($sp)
-    lw $a1, 36($sp)
-    lw $a2, 40($sp)
-    lw $a3, 44($sp)
-    addi $sp, $sp, 48
+    	lw $t0, 0($sp)
+    	lw $t1, 4($sp)
+    	lw $t2, 8($sp)
+    	lw $t3, 12($sp)
+    	lw $t4, 16($sp)
+    	lw $t5, 20($sp)
+    	lw $t6, 24($sp)
+    	lw $t7, 28($sp)
+    	lw $a0, 32($sp)
+    	lw $a1, 36($sp)
+    	lw $a2, 40($sp)
+    	lw $a3, 44($sp)
+    	addi $sp, $sp, 48
 .end_macro
 
-	int_to_str_buffer: .space 4
 	
 .macro voltar
 	unstack_reg
@@ -43,7 +58,7 @@
 .end_macro
 
 .text
-.globl criar_condominio, strcmp, strncmp, memcpy, int_to_str, int_to_str_buffer, str_to_int, get_tamanho_str, get_indice_ap 
+.globl criar_condominio, strcmp, strncmp, memcpy, int_to_str, int_to_str_buffer, str_to_int, get_tamanho_str, get_indice_ap, get_funcao, processar_comando, prox_linha, free 
 	
 
 
@@ -144,8 +159,26 @@ strncmp:
 
 				
 
-# Falta o memcpy (Falar com samara depois)
+# Funcao memcpy -> copia um total de bytes dado um número do local e salva em memoria
+# $a0 -> destino
+# $a1 -> source
+# $a2 -> numero
 memcpy:
+	stack_reg
+	move $t0, $a0          			# $t0 -> destino
+	move $t1, $a1          			# $t1 -> source
+	move $t2, $a2          			# $t2 -> num
+
+	loop:
+    		beq $t2, $zero, voltar   	# se num == 0, fim
+    		lb $t3, 0($t1)       	 	# carrwga 1 byte em surce
+    		sb $t3, 0($t0)        		# armazena 1 byte em destino
+    		addi $t0, $t0, 1
+    		addi $t1, $t1, 1
+    		addi $t2, $t2, -1     		# decrementa 1
+    		j loop
+
+
 
 
 
@@ -208,22 +241,24 @@ str_to_int:
         		voltar
 
 
-
+# Funcao get_tamanho_str -> Le de byte a byte para contar qual o tamanho da string
+# $a0 -> string a ser lida
+# $v0 -> resultado em inteiro do tamanho da string
 get_tamanho_str:
 	stack_reg
-    	move $t0, $a0                               	# Copy the string address to $t0
-   	li $t1, 0                                   	# Initialize the counter to 0
+    	move $t0, $a0                               	# Copia o valor da string 
+   	li $t1, 0                                   	# Inicializa o contador pra 0
 
     	loop_tamanho_str:      
-       		lb $t2, 0($t0)                          	# Load the next character into $t2
-        		beq $t2, $zero, fim_funcao       		# If the character is null, exit the loop
-       		beq $t2, 10, fim_funcao          		# If the character is \n, exit the loop
-        		addi $t0, $t0, 1                        	# Increment the address of the string
-        		addi $t1, $t1, 1                        	# Increment the counter
-        		j loop_tamanho_str                           	# Jump back to the start of the loop
+       		lb $t2, 0($t0)                          	# Carrega o proximo caractere a ser lido em $t2
+        		beq $t2, $zero, fim_funcao       		# Se o caractere e nulo, entao acabou 
+       		beq $t2, 10, fim_funcao          		# Se o caractere e \n, entao acabou
+        		addi $t0, $t0, 1                        	# Pula pro proximo byte
+        		addi $t1, $t1, 1                        	# +1 pro contador
+        		j loop_tamanho_str                       	# Volta o loop ate acabar
 
     	fim_funcao:
-       		move $v0, $t1                           	# Set the function return value to the size of the string
+       		move $v0, $t1                           	# Retorna o valor da string em $v0
 		
 		voltar
 
@@ -240,8 +275,8 @@ get_indice_ap:
 	add $t0, $a0, $zero
 	li $t1, 100
 	div $t0, $t1	
-	mflo $t2					# indice andar
-	mfhi $t4					# indice apartamento
+	mflo $t2					# Indice andar
+	mfhi $t4					# Indice apartamento
 	
 	# Validar se o andar e valido
 	blez $t2, indice_invalido		# Se o andar e o 0, entao é invalido
@@ -271,6 +306,158 @@ get_indice_ap:
 		jr $ra
 	
 
+
+# Funcao get_funcao ->
+get_funcao:
+	stack_reg
+	add $t0, $zero, $a1
+	la $t1, separador
+	lb $t1, 0($t1)
+	add $t2, $a0, $zero
+	
+	procurar_hifen:
+		lb $t3, 0 ($t0)
+		addi $t0, $t0, 1
+		beqz $t3, fim_get_funcao
+		bne $t3, $t1, procurar_hifen
+		addi $t2, $t2, -1
+		beqz $t2, guardar_funcao
+		j procurar_hifen
+		
+	guardar_funcao:
+		addi $t2, $t0, 0
+		achar_funcao:
+			lb $t3, 0($t2)
+			addi $t2, $t2, 1
+			beqz $t3, if_achar_funcao
+			bne $t3, $t1, achar_funcao
+			addi $t2, $t2, 1
+		
+		if_achar_funcao:
+			sub $t2, $t2, $t0                                           
+            		addi $t2, $t2, 1                                            
+            		add $a0, $zero, $t2                                        
+            		addi $v0, $zero, 9                                         
+            		syscall                                                    
+            		add $v1, $zero, $t2                                         
+            		addi $t2, $t2, -1                                           
+            		add $a2, $zero, $t2                                       
+            		add $a1, $zero, $t0                                         
+            		add $a0, $zero, $v0                                         
+            		addi $sp, $sp, -4                                          
+            		sw $ra, 0($sp)                                             
+            		jal memcpy                                                 
+            		lw $ra, 0($sp)                                            
+            		addi $sp, $sp, 4                                           
+            		unstack_reg                                                                
+           		jr $ra                                                      
+
+	
+	fim_get_funcao:
+		unstack_reg
+		j op_invalida
+		jr $ra
+	
+	
+# Funcao processar_comando ->
+#
+#
+processar_comando:
+	stack_reg                                                               	
+    	la $t0, input                                                       	
+    	lb $t1, 0($t0)
+    	beqz $t1, fim_processar
+    	addi $t1, $zero, -1                                               		
+    	la $t3, separador                                                   	
+    	lb $t3, 0($t3)
+    	loop_processar:                                                         	
+        		lb $t2, 0($t0)                                                  	
+        		beqz $t2, comparar_cmd                                               	
+        		beq $t2, $t3, comparar_cmd                                           	
+        		addi $t0, $t0, 1                                                
+        		addi $t1, $t1, 1                                                	
+        		j loop_processar
+        		
+        		  
+        	# Switch case para saber qual o comando que foi digitado pelo usuario
+        		                                                   
+    	comparar_cmd:                                                      
+        		la $a0, ad_morador      				# Carrega para ver se é 'ad_morador'                                       
+        		li $a3, 10                                        # Tamanho do comando a ser comparado            
+        		jal strncmp                                       # Vai pro strncmp para ver ser é a mesma string  
+        		beqz $v0, ad_morador_fn                           # Caso foi, ele redireciona pra funcao do comando
+		
+		la $a0, ad_auto                                 	# Caso 'ad_auto'                
+        		li $a3, 7
+        		jal strncmp                                                     
+        		beqz $v0, ad_auto_fn                                       
+        		
+        		la $a0, rm_morador                             	# Caso 'rm_morador'                
+        		li $a3, 10
+        		jal strncmp                                                   
+        		beqz $v0, rm_morador_fn                                         
+
+        		la $a0, rm_auto                            	# Caso 'rm_auto'                     
+        		li $a3, 7
+        		jal strncmp                                                     
+        		beqz $v0, rm_auto_fn                                            
+        		
+        		la $a0, info_ap                          		# Caso 'info_ap'                      
+        		li $a3, 7
+        		jal strncmp                                                     
+        		beqz $v0, info_ap_fn                           		                
+
+        		la $a0, limpar_ap                			# Caso 'limpar_ap'                               
+        		li $a3, 9
+        		jal strncmp                                                     
+        		beqz $v0, limpar_ap_fn                                          
+
+        		la $a0, recarregar    				# Caso 'recarregar'                                          
+        		li $a3, 10
+        		jal strncmp                                                     
+        		beqz $v0, recarregar_arquivo                                        
+
+		la $a0, salvar                                    # Caso 'salvar'            
+        		li $a3, 6
+        		jal strncmp                                                     
+        		beqz $v0, salvar_arquivo                                        
+        		
+        		la $a0, formatar                                 	# Caso 'formatar'          
+        		li $a3, 8
+        		jal strncmp                                                     
+       		beqz $v0, formatar_arquivo                                           
+        
+        		j cmd_invalido_fn                                 # Caso não tenha nenhum acesso, traz a funcao de cmd invalido default
+    
+    fim_processar:                                                     
+        unstack_reg
+        j start                                                   # Volta pro inicio, caso chamado novamente, volta pro inicio
+
+
+prox_linha:
+    li   $t9, 0                    		# Inicializa contador de bytes (tamanho da linha)
+
+	loop_prox_linha:
+    		lb   $t7, 0($t0)              	# Carrega byte atual do buffer em $t7
+    		beq  $t7, 10, fim_linha    	# Se for '\n', finaliza busca
+    		addi $t9, $t9, 1              	# Incrementa contador de caracteres
+    		addi $t0, $t0, 1             	# Avança ponteiro no buffer
+    		j   loop_prox_linha             	# Continua buscando
+
+	fim_linha:
+    		addi $t0, $t0, 1              # Move ponteiro para depois do '\n'
+    		move $v0, $t9                 # Retorna tamanho da linha
+    		jr   $ra                      # Retorna da função
+
+
+#Funcao free -> 
+free:
+	stack_reg
+	lb $t0, 0($a0)
+	sb $zero, 0($a0)
+	addi $a0, $a0, 1
+	beqz $t0, voltar
+	j free
 
 
 	
