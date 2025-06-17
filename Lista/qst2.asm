dcd6e163d03a89936b2beb96d30ec25aa56219b0
V1Barros <167831562+V1Barros@users.noreply.github.com>
2025-06-17 00:33:59 -0300

    cabeçalho denovo

--- Lista/qst2.asm
+++ Lista/qst2.asm
@@ -3,7 +3,6 @@
 # Samara Accioly
 # Vitor Barros de Carvalho
 # Wellington Viana da Silva Junior
-# Arquivo referente as funcoes de comando do projeto
 
 .data
 io_control: .word 0xffff                    # endereco base dos registradores MMIO
