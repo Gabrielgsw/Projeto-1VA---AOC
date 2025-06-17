a7b17b0353c5411d1997a16893ef2559f0ca09fe
Samara Accioly <134091369+Samaraaccioly@users.noreply.github.com>
2025-06-17 12:06:13 -0300

    Update memcpyLista.asm

--- Lista/memcpyLista.asm
+++ Lista/memcpyLista.asm
@@ -25,27 +25,22 @@ main:
     # Finaliza o programa
     li $v0, 10
     syscall
+    
+# função memcpy
+memcpy:
+    move $t0, $a0
+    move $t1, $a1
+    move $t2, $a2
 
+loop:
+    beq $t2, $zero, end
+    lb $t3, 0($t1)
+    sb $t3, 0($t0)
+    addi $t0, $t0, 1
+    addi $t1, $t1, 1
+    addi $t2, $t2, -1
+    j loop
 
-memcpy:                                 
-	add $t0, $zero, $a0                 	#escreve o endereço destino para t0
-    	add $t1, $zero, $a1                 	#escreve o endereço source para t1
-    	add $t2, $zero, $zero               	#escreve 0 em t2 para o contador
-    	addi $t4, $zero, 10			#armazena '\n' em ASCII para o fim da operação
-
-	loop:
-    		bge $t2, $a2, end_memcpy        	#caso i seja igual a num, encerra a função
-        		lb $t3, 0($t1)                  	#carrega o byte de source em t3
-        		beq $t3, $t4, end_memcpy		#se o byte for igual a 10('\n') -> fim
-        		sb $t3, 0($t0)                  	#grava o byte de t3 em destination
-        		addi $t0, $t0, 1                	#incrementa destination
-        		addi $t1, $t1, 1                	#incrementa source
-        		addi $t2, $t2, 1                	#incrementa i
-       	 	
-       	 	j loop                   #refaz o loop
-
-	
-	end_memcpy:
-		addi $t0, $t0, 1
-        		sb $zero, 0($t0)
-        		jr $ra
+end:
+    move $v0, $a0
+    jr $ra
